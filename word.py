5d3e41301b09ecd5a890466caad7f1f2390e53bf
Charles de Breteuil <charles2bret@gmail.com>
2018-08-21 20:59:15 -0400

    more functions

--- word.py
+++ word.py
@@ -4,6 +4,8 @@ class Word(object):
         super(Word, self).__init__()
         self.length = len(hangman_word)
         self.characters = dict()
+        self.guessed_letters = 0
+        self.guess = ['_']*int(self.length)
         for i in range(self.length):
 
             if self.characters.get(hangman_word[i]) is None:
@@ -11,7 +13,25 @@ class Word(object):
                 self.characters[hangman_word[i]].append(i)
             else:
                 self.characters[hangman_word[i]].append(i)
-        print(self.characters)
+
+    def get_guess(self):
+        return self.guess
+
+    def has_letter(self, letter):
+        return self.characters.get(letter) is not None
+
+    def update_guess(self, letter):
+        try:
+            self.guessed_letters += int(len(self.characters[letter]))
+            for i in self.characters[letter]:
+                self.guess[i] = letter
+        except KeyError:
+            print("unexpected error")
 
 
-myword = Word("abracadabra")
+
+other = Word("unitedkingdom")
+print(other.has_letter('z'))
+other.update_guess('k')
+print(other.get_guess())
+print(other.guessed_letters)
