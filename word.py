3f837c014a56746e16e63dd6c07b6899ce9194ef
Charles de Breteuil <charles2bret@gmail.com>
2018-08-21 21:09:57 -0400

    got rid of useless getter and better varnames

--- word.py
+++ word.py
@@ -4,7 +4,7 @@ class Word(object):
         super(Word, self).__init__()
         self.length = len(hangman_word)
         self.characters = dict()
-        self.guessed_letters = 0
+        self.number_of_guessed_letters = 0
         self.guess = ['_']*int(self.length)
         for i in range(self.length):
 
@@ -14,24 +14,25 @@ class Word(object):
             else:
                 self.characters[hangman_word[i]].append(i)
 
-    def get_guess(self):
-        return self.guess
-
     def has_letter(self, letter):
         return self.characters.get(letter) is not None
 
     def update_guess(self, letter):
         try:
-            self.guessed_letters += int(len(self.characters[letter]))
+            self.number_of_guessed_letters += int(len(self.characters[letter]))
             for i in self.characters[letter]:
                 self.guess[i] = letter
         except KeyError:
             print("unexpected error")
 
+    def is_guessed(self):
+        return self.length == self.number_of_guessed_letters
 
 
-other = Word("unitedkingdom")
+
+other = Word("uuuuuuu")
 print(other.has_letter('z'))
-other.update_guess('k')
-print(other.get_guess())
-print(other.guessed_letters)
+other.update_guess('u')
+print(other.guess)
+print(other.number_of_guessed_letters)
+print(other.is_guessed())
